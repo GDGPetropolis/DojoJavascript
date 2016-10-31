46449dbec9be6c40abf77743c5382f75dd9a5d2e
Ricardo Stefani <ricardostefanisantos@gmail.com>
2016-10-31 13:13:18 -0200

    show result fizzbuzz in html

--- js/main.js
+++ js/main.js
@@ -2,9 +2,25 @@
 {
     app.loadScript("js/dojo/dojo.js");
 
-    app.main = function() {
-            const dojo = new app.Dojo('#app-fizzbuzz');
+    app.fizzbuzz_html = function(start, stop)
+    {
+		const dojo = new app.Dojo();
 
-            dojo.fizzBuzz(1, 101);
+        let result_fizzbuzz = document.querySelectorAll('#app-fizzbuzz .result')[0];
+
+        result_fizzbuzz.innerHTML = "";
+
+        let list = document.createElement('ul');
+
+        for (let number = start; number <= stop; number++)
+        {
+            let item = document.createElement('li');
+
+            item.innerHTML = dojo.fizzBuzz(number);
+
+            list.appendChild(item);
+        }
+
+        result_fizzbuzz.appendChild(list);
     }
 })(window.app);
