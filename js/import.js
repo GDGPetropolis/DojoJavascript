eef50e475e05f05e3a0e44bd8f9631befb86b2cf
Ricardo Stefani <ricardostefanisantos@gmail.com>
2016-10-31 13:14:38 -0200

    just one more commit

--- js/import.js
+++ js/import.js
@@ -3,13 +3,20 @@
     app.loadScript = function(url, callback)
     {
         let head = document.getElementsByTagName('head')[0];
+
         let script = document.createElement('script');
+
         script.type = 'text/javascript';
+
         script.src = url;
+
         script.onreadystatechange = callback;
+
         script.onload = callback;
+
         head.appendChild(script);
     }
 
     app.loadScript("js/main.js");
+
 })(window.app || (window.app = {}));
